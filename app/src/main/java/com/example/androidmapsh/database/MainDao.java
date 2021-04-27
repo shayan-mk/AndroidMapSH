e051441d4f5f41d10a8a5c17127bd0306d69cd2f
ShayanTalaei <talaee.shayan@gmail.com>
2021-04-27 17:41:51 +0430

    MainDao.database -> implementation

---
+++ app/src/main/java/com/example/androidmapsh/database/MainDao.java
@@ -0,0 +1,35 @@
+package com.example.androidmapsh.database;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public interface MainDao {
+    //Insert query
+    @Insert(onConflict = REPLACE)
+    void insert(Location location);
+
+    //Delete query
+    @Delete
+    void delete(Location location);
+
+    //Delete all queries
+    @Delete
+    void reset(List<Location> locations);
+
+    //Update query
+    @Query("UPDATE Location SET name = :sName WHERE ID = :sID")
+    void update(int sID, String sName);
+
+    //Get all data query
+    @Query("SELECT * FROM Location")
+    List<Location> getAll();
+
+}
--- app/src/main/java/com/example/androidmapsh/database/RoomDB.java
+++ app/src/main/java/com/example/androidmapsh/database/RoomDB.java
@@ -27,7 +27,8 @@ public abstract class RoomDB extends RoomDatabase{
         return database;
     }
 
-    //
+    //Create Dao
+    public abstract MainDao mainDao();
 
 
 }
