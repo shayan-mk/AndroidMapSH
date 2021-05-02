89ad415f024a8a23ed6ef5d1184e1ed69da79c6c
ShayanTalaei <talaee.shayan@gmail.com>
2021-05-02 19:27:28 +0430

    SearchFragment

--- app/src/main/java/com/example/androidmapsh/map/NetworkManager.java
+++ app/src/main/java/com/example/androidmapsh/map/NetworkManager.java
@@ -1,4 +1,12 @@
 package com.example.androidmapsh.map;
 
+import java.util.List;
+
 public class NetworkManager {
+
+
+    //TODO
+    public static List<String> getRecommendations(String text){
+        return null;
+    }
 }
---
+++ app/src/main/java/com/example/androidmapsh/ui/map/SearchFragment.java
@@ -0,0 +1,47 @@
+package com.example.androidmapsh.ui.map;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.androidmapsh.MainActivity;
+import com.example.androidmapsh.R;
+
+import java.util.ArrayList;
+
+public class SearchFragment extends Fragment {
+    //Initialize variables
+    TextView textView;
+    ArrayList<String> recommendations;
+    Dialog dialog;
+
+    public View onCreate(@NonNull LayoutInflater inflater,
+                         ViewGroup container, Bundle savedInstanceState){
+
+        //Assign variavle
+        View root = inflater.inflate(R.layout.search_box, container, false);
+        final TextView textView = root.findViewById(R.id.text_view);
+
+        //Initialize array list
+        recommendations = new ArrayList<>();
+        //TODO: fill the array list
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Initialize dialog
+                //TODO: cannot use this fragment
+                //dialog = new Dialog(MainActivity.this);
+
+            }
+        });
+
+        return root;
+    }
+}
